b013888d4d58b008e819dc489454e9c430a19c14
Sujit <sjoshi1@saic.edu>
2022-05-27 00:37:48 +0530

    c

--- index-script.js
+++ index-script.js
@@ -6,18 +6,18 @@ var observer = new IntersectionObserver(function (entries) {
         document.getElementById('hidden-logo').style.opacity = '100';
         document.getElementById('hidden-button').style.opacity = '100';
         document.getElementById('sticky-navcontainer').style.display = 'flex';
-        document.getElementById('banner-spacer').style.display='none';
+        document.getElementById('banner-spacer').style.display = 'none';
     }
 
     // fully intersects with screen
     else if (entries[0].intersectionRatio === 1) {
         document.getElementById('hidden-logo').style.opacity = '0';
         document.getElementById('hidden-button').style.opacity = '0';
-        if(window.innerWidth < 1236){
+        if (window.innerWidth < 1236) {
             document.getElementById('sticky-navcontainer').style.display = 'none';
         }
-        
-        document.getElementById('banner-spacer').style.display='block';
+
+        document.getElementById('banner-spacer').style.display = 'block';
     }
 
 }, {
@@ -29,46 +29,74 @@ observer.observe(document.getElementById('banner-button'));
 
 
 
-let exampleImg = 2;
+let exampleImg = 1;
+
 
 
 function exampleRightPress() {
     exampleImg++;
-    if (exampleImg == 8) {
+    if (exampleImg == 9) {
         exampleImg = 1;
     }
 
-    for (let i = 1; i <= 7; i++) {
+    for (let i = 1; i <= 8; i++) {
         document.getElementById('example-img-' + i).style.display = 'none';
         document.getElementById('example-indi-' + i).className = 'indicator-deselected';
     }
+
+    let jobName = document.getElementById('job-name');
+    let pages = document.getElementById('pages');
+    let innerPaper = document.getElementById('inner-paper');
+    let coverPaper = document.getElementById('cover-paper');
+    let binding = document.getElementById('binding');
+    let finish = document.getElementById('finish');
+
     document.getElementById('example-img-' + exampleImg).style.display = 'block';
     document.getElementById('example-indi-' + exampleImg).className = 'indicator-selected';
-   
-    
+    jobName.innerHTML = document.getElementById('example-img-' + exampleImg).getAttribute('bookname');
+    pages.innerHTML = document.getElementById('example-img-' + exampleImg).getAttribute('details');
+    innerPaper.innerHTML = 'Inner pages: ' + document.getElementById('example-img-' + exampleImg).getAttribute('inner-paper');
+    coverPaper.innerHTML ='Cover: ' +  document.getElementById('example-img-' + exampleImg).getAttribute('cover');
+    binding.innerHTML ='Binding: ' +  document.getElementById('example-img-' + exampleImg).getAttribute('binding');
+    finish.innerHTML ='Finish: ' +  document.getElementById('example-img-' + exampleImg).getAttribute('finish');
 
 }
 
 function exampleLeftPress() {
     exampleImg--;
     if (exampleImg == 0) {
-        exampleImg = 7;
+        exampleImg = 8;
     }
-    for (let i = 1; i <= 7; i++) {
+    for (let i = 1; i <= 8; i++) {
         document.getElementById('example-img-' + i).style.display = 'none';
         document.getElementById('example-indi-' + i).className = 'indicator-deselected';
     }
     document.getElementById('example-img-' + exampleImg).style.display = 'block';
     document.getElementById('example-indi-' + exampleImg).className = 'indicator-selected';
-    
-    
+
+    let jobName = document.getElementById('job-name');
+    let pages = document.getElementById('pages');
+    let innerPaper = document.getElementById('inner-paper');
+    let coverPaper = document.getElementById('cover-paper');
+    let binding = document.getElementById('binding');
+    let finish = document.getElementById('finish');
+
+    document.getElementById('example-img-' + exampleImg).style.display = 'block';
+    document.getElementById('example-indi-' + exampleImg).className = 'indicator-selected';
+    jobName.innerHTML = document.getElementById('example-img-' + exampleImg).getAttribute('bookname');
+    pages.innerHTML = document.getElementById('example-img-' + exampleImg).getAttribute('pages');
+    innerPaper.innerHTML = 'Inner pages: ' + document.getElementById('example-img-' + exampleImg).getAttribute('inner-paper');
+    coverPaper.innerHTML ='Cover: ' +  document.getElementById('example-img-' + exampleImg).getAttribute('cover');
+    binding.innerHTML ='Binding: ' +  document.getElementById('example-img-' + exampleImg).getAttribute('binding');
+    finish.innerHTML ='Finish: ' +  document.getElementById('example-img-' + exampleImg).getAttribute('finish');
+
 }
 
 
 
 
 
-let recogImg = 2;
+let recogImg = 1;
 
 
 function recogRightPress() {
@@ -83,7 +111,7 @@ function recogRightPress() {
     }
     document.getElementById('recog-img-' + recogImg).style.display = 'block';
     document.getElementById('recog-indi-' + recogImg).className = 'indicator-selected';
-   
+
 
 }
 
@@ -92,14 +120,14 @@ function recogLeftPress() {
     if (recogImg == 0) {
         recogImg = 3;
     }
-    
+
     for (let i = 1; i <= 3; i++) {
         document.getElementById('recog-img-' + i).style.display = 'none';
         document.getElementById('recog-indi-' + i).className = 'indicator-deselected';
     }
     document.getElementById('recog-img-' + recogImg).style.display = 'block';
     document.getElementById('recog-indi-' + recogImg).className = 'indicator-selected';
-    
+
 }
 
 let quoteText = 1;
@@ -131,6 +159,6 @@ function quoteLeftPress() {
     document.getElementById('quote-indi-' + quoteText).className = 'indicator-selected';
 }
 
-function mailUnited(){
-    window.location.href='mailto:unitedprinters@rediffmail.com';
+function mailUnited() {
+    window.location.href = 'mailto:unitedprinters@rediffmail.com';
 }
